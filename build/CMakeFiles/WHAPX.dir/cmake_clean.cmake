7a7312da8eb10115865c9dfbe0cd8a05a0f088a7
InsydeHere20 <57913632+InsydeHere20@users.noreply.github.com>
2022-11-26 22:50:43 +0100

    SFQSEFQ

---
+++ build/CMakeFiles/WHAPX.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WHAPX.dir/main.cpp.o"
+  "CMakeFiles/WHAPX.dir/main.cpp.o.d"
+  "WHAPX"
+  "WHAPX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WHAPX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
